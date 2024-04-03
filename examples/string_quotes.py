f0a37ab7ca70da4216758122b2f6ce20fef8b2d0
Akarsh Kumar Saraff <saraffakarsh@gmail.com>
2024-04-03 12:23:45 +0530

    string quotes

---
+++ examples/string_quotes.py
@@ -0,0 +1,16 @@
+
+# single quotes for words
+word = 'Dog'
+print(word)
+
+# groupf odf words
+groupOfWords = ". It's my favourite pet,"
+print(groupOfWords)
+
+# multiline paragraph
+paragraph = """ I love my dog
+more than any one else."""
+print(paragraph)
+
+# combined
+print(word+groupOfWords+paragraph)
