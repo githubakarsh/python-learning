e6a3606c508cd81e8b68d146e72348212c9a805f
Akarsh Kumar Saraff <saraffakarsh@gmail.com>
2024-04-03 12:28:29 +0530

    line continuity

---
+++ examples/line_continuation.py
@@ -0,0 +1,13 @@
+# line continuation for numbers
+sum = 1 + \
+2 + \
+3
+
+#print sum 
+print(sum)
+
+# line continuation for arrays or lists
+
+vowels = ['a', 'e',
+          'i', 'o', 'u']
+print(vowels)
